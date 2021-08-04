22a95cee133249a4ed983c6b3491d3498d71be9a
Mark-abdallah <83455664+Mark-abdallah@users.noreply.github.com>
2021-08-04 11:34:49 +0200

    Repository

---
+++ noonBack/RepoL/Repository pattern/Repository.cs
@@ -0,0 +1,82 @@
+﻿using DAL;
+using DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepoL.Repository_pattern
+{
+    public class Repository<T> : IRepository<T> where T : Product
+    {
+        #region properties
+        private readonly ApplicationDbContext _applicationDbContext;
+        private DbSet<T> entities;
+        #endregion
+
+        #region constructor
+        public Repository(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+            entities = _applicationDbContext.Set<T>();
+        }
+        #endregion
+        public void Delete(T entity)
+        {
+            if (entity==null)
+            {
+                throw new ArgumentNullException("entity is null");
+            }
+            entities.Remove(entity);
+            _applicationDbContext.SaveChanges();
+        }
+
+        public T Get(int Id)
+        {
+            return entities.SingleOrDefault(c => c.Id == Id);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return entities.AsEnumerable();
+        }
+
+        public void Insert(T entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException("entity is null");
+            }
+            entities.Add(entity);
+            _applicationDbContext.SaveChanges();
+        }
+
+        public void Remove(T entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException("entity in null");
+            }
+            entities.Remove(entity);
+            _applicationDbContext.SaveChanges();
+        }
+
+        public void SaveChanges()
+        {
+            _applicationDbContext.SaveChanges();
+        }
+
+        public void Update(T entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException("entity is null");
+            }
+            entities.Update(entity);
+            _applicationDbContext.SaveChanges();
+
+        }
+    }
+}
