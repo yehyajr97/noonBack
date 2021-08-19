ad7830489b2bf81f036946ed403ae39813b773f2
Mark-abdallah <83455664+Mark-abdallah@users.noreply.github.com>
2021-08-19 14:27:36 +0200

    byName

--- noonBack/DAL/Models/Product.cs
+++ noonBack/DAL/Models/Product.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace DAL.Models
@@ -23,13 +24,18 @@ namespace DAL.Models
         public string SellerId { get; set; }
         public virtual ICollection<Review> Review { get; set; }
         public virtual ICollection<ProductImages> ProductImages { get; set; }
+        [JsonIgnore]
         public virtual  Order Order { get; set; }
+        [JsonIgnore]
         [ForeignKey("CategoryId")]
         public virtual Category Category { get; set; }
+        [JsonIgnore]
         [ForeignKey("BrandId")]
         public virtual Brand Brand { get; set; }
+        [JsonIgnore]
         [ForeignKey("SubCategoryId")]
         public virtual SubCategory SubCategory { get; set; }
+        [JsonIgnore]
         [ForeignKey("SellerId")]
         public virtual ApplicationUser Seller { get; set; }
 
